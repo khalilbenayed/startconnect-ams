b983eba0ad19f2f802a63613f21e5b6136e93405
khalilbenayed <kbenayed@edu.uwaterloo.ca>
2019-07-11 22:13:50 -0400

    Running with gunicorn

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ def create_app(config_name):
     return app, api
 
 
+app, api = create_app(__name__)
+
 if __name__ == '__main__':
-    app, api = create_app(__name__)
     app.run(debug=True, host='0.0.0.0', port=5000)
